c4406547c5c43838e76583ae5b7669699454901a
Jason Cameron <jasoncameron246@gmail.com>
2023-09-25 12:14:53 -0400

    refactor code to render individual product.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
             <nav class="container mx-auto p-4 flex justify-between">
                 <NuxtLink to="/" class="font-bold">Demo Shop</NuxtLink>
                 <ul class="flex gap-4">
-                    <li><Nuxt-link to="/">Home</Nuxt-link></li>
-                    <li><Nuxt-link to="/about">About</Nuxt-link></li>
-                    <li><Nuxt-link to="/products">Products</Nuxt-link></li>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
                 </ul>
             </nav>
         </header>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>
-        prodcurts page.
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="product.id">
+        <ProductCard :product="product" />
+      </div>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
